4a78256815f341b44403ef61def9d83089f9fe3e
Patrick El-Azem <paelaz@microsoft.com>
2021-02-10 10:59:11 -0500

    cleanup

--- CosmosDbDynamic/Source/CosmicDynamic/Program.cs
+++ CosmosDbDynamic/Source/CosmicDynamic/Program.cs
@@ -5,6 +5,8 @@ using System.Configuration;
 using System.Dynamic;
 using System.Linq;
 using System.Net;
+using System.Reflection;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.Cosmos;
@@ -17,49 +19,59 @@ namespace CosmicDynamic
 	class Program
 	{
 		// SQL or Graph account values
-		private static string _endpointUrl = "https://PROVIDE.documents.azure.com:443/";
-		private static string _key = "PROVIDE";
-		private static string _databaseId = "PROVIDE";
-		private static string _containerId = "PROVIDE";
+		private static string _endpointUrl = "";
+		private static string _key = "";
+		private static string _databaseId = "";
+		private static string _containerId = "";
 
 		public static async Task Main(string[] args)
 		{
-			// What properties do we want to see on the output
-			List<string> outputPropertyNames = new List<string>() { "id", "firstname", "lastname", "email" };
-
-			IEnumerable<JObject> documents = await GetDocuments(_endpointUrl, _key, _databaseId, _containerId, outputPropertyNames);
+			IEnumerable<JObject> documents = await GetDocuments(_endpointUrl, _key, _databaseId, _containerId).ConfigureAwait(false);
 
 			IEnumerable<dynamic> results = ProcessDocuments(documents);
 
+			foreach (JObject document in documents)
+			{
+				string djson = document.ToString();
+				//string djson = JsonConvert.SerializeObject(document);
+
+				Console.WriteLine(djson);
+				Console.WriteLine();
+			}
+
+			foreach (dynamic result in results)
+			{
+				Console.WriteLine(result.id);
+			}
+
 			// OK, we have our results as an IEnumerable<dynamic>... now let's do something useful with the results.
 			// Here I'm just sending each to console with a set of key=value pairs so we can see what the dynamic contains.
-			foreach (dynamic result in results)
-				Console.WriteLine((result as IDictionary<string, object>)?.GetDelimitedList(" | ", "Doh! Nothing there!"));
+			//foreach (dynamic result in results)
+			//	Console.WriteLine((result as IDictionary<string, object>)?.GetDelimitedList(" | ", "Doh! Nothing there!"));
 
 			Console.WriteLine();
 			Console.WriteLine("Done - hit any key to exit.");
 			Console.ReadKey();
 		}
 
-		private static async Task<IEnumerable<JObject>> GetDocuments(string endpointUrl, string key, string databaseId, string containerId, IEnumerable<string> outputPropertyNames = null)
+		private static async Task<IEnumerable<JObject>> GetDocuments(string endpointUrl, string key, string databaseId, string containerId)
 		{
 			List<JObject> results = new List<JObject>();
 
 			using (CosmosClient client = new CosmosClient(endpointUrl, key))
 			{
 				Database cosmosDatabase = client.GetDatabase(databaseId);
-
 				Container container = cosmosDatabase.GetContainer(containerId);
 
-				string queryText = GetQueryText(outputPropertyNames);
-
-				FeedIterator<JObject> query = container.GetItemQueryIterator<JObject>(queryText);
+				FeedIterator<JObject> query = container.GetItemQueryIterator<JObject>();
 
 				try
 				{
 					while (query.HasMoreResults)
 					{
-						foreach (JObject result in await query.ReadNextAsync())
+						FeedResponse<JObject> currentResultSet = await query.ReadNextAsync().ConfigureAwait(false);
+
+						foreach (JObject result in currentResultSet)
 							results.Add(result);
 					}
 				}
@@ -72,43 +84,70 @@ namespace CosmicDynamic
 			return results;
 		}
 
-		private static string GetQueryText(IEnumerable<string> outputPropertyNames = null)
-		{
-			// Prepare query text in case we want to project Azure-side
-			string queryText = null;
+		//private static string GetQueryText(IEnumerable<string> outputPropertyNames = null)
+		//{
+		//	// Prepare query text in case we want to project Azure-side
+		//	string queryText = null;
 
-			if (outputPropertyNames != null && outputPropertyNames.Count() > 0)
-			{
-				// Project Azure-side and pass less data back over the wire
-				string projection = outputPropertyNames.Select(opn => $"c.{opn}").GetDelimitedList(", ", string.Empty, false);
+		//	if (outputPropertyNames != null && outputPropertyNames.Count() > 0)
+		//	{
+		//		// Project Azure-side and pass less data back over the wire
+		//		string projection = outputPropertyNames.Select(opn => $"c.{opn}").GetDelimitedList(", ", string.Empty, false);
 
-				queryText = $"SELECT {projection} FROM c";
-			}
-			else
-			{
-				// No Azure-side projection - we'll just retrieve entire documents
-				queryText = null;
-			}
+		//		queryText = $"SELECT {projection} FROM c";
+		//	}
+		//	else
+		//	{
+		//		// No Azure-side projection - we'll just retrieve entire documents
+		//		queryText = null;
+		//	}
 
-			return queryText;
-		}
+		//	return queryText;
+		//}
 
 		private static IEnumerable<dynamic> ProcessDocuments(IEnumerable<JObject> documents)
 		{
+			List<dynamic> results = new List<dynamic>();
+
 			foreach (JObject document in documents)
 			{
-				dynamic result = new ExpandoObject();
-				IDictionary<string, object> resultAsDictionary = result as IDictionary<string, object>;
+				IEnumerable<JProperty> props = document.Properties().Where(p => !(p.Name.StartsWith("_", StringComparison.InvariantCultureIgnoreCase)));
 
-				foreach (JToken jToken in document.Values())
-					resultAsDictionary.Add
-					(
-						jToken.Path,
-						(jToken.HasValues ? jToken.First.Value<string>("_value") : jToken.Value<string>())
-					);
+				foreach (JProperty prop in props)
+				{
+					if (prop.Value.HasValues)
+					{
+						// It's a JArray
+						JArray array = new JArray(prop.Value);
 
-				yield return result;
+						Console.WriteLine("Array: " + prop.Name + " | " + prop.Count.ToString());
+					}
+					else
+					{
+						// It's a scalar
+
+						Console.WriteLine("Scalar: " + prop.Name + " = " + prop.Value.ToString());
+					}
+				}
+
+				results.Add(document as dynamic);
+
+				//dynamic result = new ExpandoObject();
+				//IDictionary<string, object> resultAsDictionary = result as IDictionary<string, object>;
+
+				//IJEnumerable<JToken> jvalues = document.Values();
+
+				//foreach (JToken jToken in jvalues)
+				//	resultAsDictionary.Add
+				//	(
+				//		jToken.Path,
+				//		(jToken.HasValues ? jToken.First.Value<string>("_value") : jToken.Value<string>())
+				//	);
+
+				//yield return result;
 			}
+
+			return results;
 		}
 	}
 }
